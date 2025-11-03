5c28e41ffc7be120f6c663b508c11ce20276ec05
Jackson Moraes <jacksontadeu@hotmail.com>
2025-11-03 12:44:04 -0300

    :sparkles: feat: Criacao das pastas de organizacao do projeto

--- frotas/backend/src/test/java/br/com/novotriunfo/frotas/ApplicationTests.java
+++ frotas/backend/src/test/java/br/com/novotriunfo/frotas/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FrotasApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
