3f37e392d7399b79d644f25f9895f8f5168fbf14
Jackson Moraes <jacksontadeu@hotmail.com>
2025-11-03 11:23:55 -0300

    :sparkles: feat: Cricao do projeto e paginas de cadastro de base e veiculos

---
+++ frotas/src/main/java/br/com/novotriunfo/frotas/controller/FrotasController.java
@@ -0,0 +1,27 @@
+package br.com.novotriunfo.frotas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrotasController {
+
+    @GetMapping("/")
+    private String paginaInicial(){
+        return "/home";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "/home";
+    }
+    @GetMapping("/base")
+    public String base(){
+        return "/cadastrar-base";
+    }
+    @GetMapping("/veiculo")
+    public String veiculo(){
+        return "cadastrar-veiculo";
+    }
+
+}
